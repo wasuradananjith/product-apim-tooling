79669026293946a27e157fd5122a281967f5d1d5
wasuradananjith <wasuradananjith@gmail.com>
2020-05-20 19:23:24 +0530

    Add docs related to importing/exporting an API Product

--- import-export-cli/shell-completions/apictl_zsh_completions.sh
+++ import-export-cli/shell-completions/apictl_zsh_completions.sh
@@ -8,7 +8,7 @@ case $state in
   level1)
     case $words[1] in
       apictl)
-        _arguments '1: :(add add-env change change-status delete export-api export-apis export-app get-keys help import-api import-app init install list login logout remove set uninstall update version)'
+        _arguments '1: :(add add-env change change-status delete export export-api export-apis export-app get-keys help import import-api import-app init install list login logout remove set uninstall update version)'
       ;;
       *)
         _arguments '*: :_files'
@@ -20,26 +20,32 @@ case $state in
       update)
         _arguments '2: :(api help)'
       ;;
-      change-status)
-        _arguments '2: :(api help)'
-      ;;
-      delete)
-        _arguments '2: :(api api-product app help)'
-      ;;
-      install)
-        _arguments '2: :(api-operator help wso2am-operator)'
-      ;;
-      list)
-        _arguments '2: :(api-products apis apps envs help)'
-      ;;
       add)
         _arguments '2: :(api help)'
       ;;
       change)
         _arguments '2: :(help registry)'
       ;;
+      change-status)
+        _arguments '2: :(api help)'
+      ;;
+      export)
+        _arguments '2: :(api-product help)'
+      ;;
       remove)
         _arguments '2: :(env help)'
+      ;;
+      delete)
+        _arguments '2: :(api api-product app help)'
+      ;;
+      import)
+        _arguments '2: :(api-product help)'
+      ;;
+      install)
+        _arguments '2: :(api-operator help wso2am-operator)'
+      ;;
+      list)
+        _arguments '2: :(api-products apis apps envs help)'
       ;;
       uninstall)
         _arguments '2: :(api-operator help wso2am-operator)'
